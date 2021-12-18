7293d008453fe2c4f2ebd0daadb79332329a4d88
nattyan-tv <undirect_sbv@yahoo.co.jp>
2021-12-18 16:43:28 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ark_server_utility
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_app(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private async void install_steamCMD(object sender, RoutedEventArgs e)
+        {
+            DialogResult dr = System.Windows.Forms.MessageBox.Show("SteamCMDのインストールには時間がかかります。\n実行してもよろしいですか？","ARK Server Utility",MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            if (dr == System.Windows.Forms.DialogResult.Cancel)
+            {
+                return;
+            }
+            if (Directory.Exists(@"SteamCMD") == false)
+            {
+                Directory.CreateDirectory(@"SteamCMD");
+                Console.WriteLine("ディレクトリ「SteamCMD」がありませんでした。");
+            }
+            else if (Directory.Exists(@"SteamCMD") == true)
+            {
+                Console.WriteLine("ディレクトリ「SteamCMD」がありました。");
+                System.Windows.Forms.MessageBox.Show("既にSteamCMDがインストールされています。", "ARK Server Utility", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+            WebClient mywebClient = new WebClient();
+            mywebClient.DownloadFile("https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip", @"SteamCMD\\steamcmd.zip");
+            try
+            {
+                ZipFile.ExtractToDirectory(@"SteamCMD\\steamcmd.zip", @"SteamCMD");
+            }
+            catch (IOException)
+            {
+                ;
+            }
+            await Task.Delay(1000);
+            File.Delete(@"SteamCMD\\steamcmd.zip");
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(@"SteamCMD\\steamcmd.exe", "+quit");
+            Process steamcmd_installer = Process.Start(processStartInfo);
+            steamcmd_installer.WaitForExit();
+            int exitCode = steamcmd_installer.ExitCode;
+            steamcmd_installer.Close();
+            Console.WriteLine("SteamCMDのインストールステータス：" + exitCode);
+            if (exitCode == 7)
+            {
+                System.Windows.Forms.MessageBox.Show("SteamCMDのインストールが完了しました。", "ARK Server Utility", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("SteamCMDのインストール作業が中断されたか、正常にインストールできませんでした。", "ARK Server Utility", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+        private void launch_steamCMD(object sender, RoutedEventArgs e)
+        {
+            if (Directory.Exists(@"SteamCMD") == false) 
+            {
+                System.Windows.Forms.MessageBox.Show("SteamCMDがインストールされていません。\n「ゲームデータ」より「SteamCMD」のインストールを選択してください。", "ARK Server Utility", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+            if (File.Exists(@"SteamCMD\\steamcmd.exe") == false)
+            {
+                System.Windows.Forms.MessageBox.Show("SteamCMDがインストールされていません。\n「ゲームデータ」より「SteamCMD」のインストールを選択してください。", "ARK Server Utility", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(@"SteamCMD\\steamcmd.exe");
+            Process.Start(processStartInfo);
+        }
+        private void uninstall_steamCMD(object sender, RoutedEventArgs e)
+        {
+            DialogResult dr = System.Windows.Forms.MessageBox.Show("SteamCMDをアンインストールしてもよろしいですか？", "ARK Server Utility", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            if (dr == System.Windows.Forms.DialogResult.Cancel)
+            {
+                return;
+            }
+            if (Directory.Exists(@"SteamCMD") == false)
+            {
+                System.Windows.Forms.MessageBox.Show("SteamCMDはインストールされていません。", "ARK Server Utility", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+            System.IO.DirectoryInfo steamcmd_del = new System.IO.DirectoryInfo(@"SteamCMD");
+            try 
+            { 
+                steamcmd_del.Delete(true);
+            }
+            catch (IOException err)
+            {
+                System.Windows.Forms.MessageBox.Show("SteamCMDのアンインストール中にエラーが発生しました。再度やり直してください。\n" + err, "ARK Server Utility", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+
+            System.Windows.Forms.MessageBox.Show("SteamCMDのアンインストールが完了しました。", "ARK Server Utility", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+
+}
