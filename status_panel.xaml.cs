3494a0008202a9712cb5c94bb50b7c11e3f43e36
nattyan-tv <undirect_sbv@yahoo.co.jp>
2022-01-17 14:18:32 +0900

    add status_panel(real)

---
+++ status_panel.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ark_server_utility
+{
+    /// <summary>
+    /// status_panel.xaml の相互作用ロジック
+    /// </summary>
+    public partial class status_panel : Window
+    {
+        public status_panel()
+        {
+            InitializeComponent();
+        }
+    }
+}
